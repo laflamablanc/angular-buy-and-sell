3f34674299164890b7f8b4ff172f4ee1cf822eb7
Sean LaFlam <sean.laflam@gmail.com>
2020-12-20 20:18:05 -0500

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsPageComponent,
+    ListingDetailPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
