adf847c42539e0479561fbf44e0608f817111684
Sean LaFlam <sean.laflam@gmail.com>
2020-12-20 20:19:08 -0500

    Created all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsComponent } from './my-listings/my-listings.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListingsPageComponent,
     ListingDetailPageComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    MyListingsComponent,
+    NewListingPageComponent,
+    EditListingPageComponent
   ],
   imports: [
     BrowserModule,
