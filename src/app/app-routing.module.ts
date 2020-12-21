d4ab95a45a00af79a53b3b6076c127b53ad8ce53
Sean LaFlam <sean.laflam@gmail.com>
2020-12-20 20:38:11 -0500

    All Routes Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { ListingsPageComponent} from './listings-page/listings-page.component'
 import { ListingDetailPageComponent} from './listing-detail-page/listing-detail-page.component'
 import { ContactPageComponent} from './contact-page/contact-page.component'
 import { EditListingPageComponent} from './edit-listing-page/edit-listing-page.component'
+import { MyListingsComponent} from './my-listings/my-listings.component'
+import { NewListingPageComponent} from './new-listing-page/new-listing-page.component'
 
 const routes: Routes = [
   {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
   {path: 'listings/:id', component: ListingDetailPageComponent},
   {path: 'contact/:id', component: ContactPageComponent},
-  {path: 'edit-listing/:id', component: EditListingPageComponent}
+  {path: 'edit-listing/:id', component: EditListingPageComponent},
+  {path: 'my-listings', component: MyListingsComponent},
+  {path: 'new-listing', component: NewListingPageComponent}
 ];
 
 @NgModule({
