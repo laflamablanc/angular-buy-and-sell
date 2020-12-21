d87aae06448b3bade598e0162eb6acaf91b5b72e
Sean LaFlam <sean.laflam@gmail.com>
2020-12-20 20:28:48 -0500

    Routes Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListingsPageComponent} from './listings-page/listings-page.component'
+import { ListingDetailPageComponent} from './listing-detail-page/listing-detail-page.component'
+import { ContactPageComponent} from './contact-page/contact-page.component'
+import { EditListingPageComponent} from './edit-listing-page/edit-listing-page.component'
 
 const routes: Routes = [
-  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'}
+  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
+  {path: 'listings/:id', component: ListingDetailPageComponent},
+  {path: 'contact/:id', component: ContactPageComponent},
+  {path: 'edit-listing/:id', component: EditListingPageComponent}
 ];
 
 @NgModule({
